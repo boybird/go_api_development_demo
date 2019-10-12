32667fa42e4b7650270a073a1e8e882288e478f5
zxk7516 <zxk7516@foxmail.com>
2019-10-12 12:57:44 +0800

    start server

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/boybird/hello/pkg/gredis"
 	"github.com/boybird/hello/pkg/logging"
 	"github.com/boybird/hello/pkg/models"
 	"github.com/boybird/hello/pkg/setting"
 	"github.com/boybird/hello/pkg/util"
+	"github.com/boybird/hello/routers"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -25,4 +30,33 @@ func init() {
 func main() {
 	gin.SetMode(setting.ServerSetting.RunMode)
 
+	routerInit := routers.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HTTPPort)
+	maxHeaderBytes := 1 << 20
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routerInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+
+	log.Printf("[info] start http server listening %s", endPoint)
+	server.ListenAndServe()
+
+	// If you want Graceful Restart, you need a Unix system and download github.com/fvbock/endless
+	//endless.DefaultReadTimeOut = readTimeout
+	//endless.DefaultWriteTimeOut = writeTimeout
+	//endless.DefaultMaxHeaderBytes = maxHeaderBytes
+	//server := endless.NewServer(endPoint, routersInit)
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
+	//}
+	//
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err: %v", err)
+	//}
 }
