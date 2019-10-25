8b3daf177bf658a20b41ed9a617709957b307fb6
zxk7516 <zxk7516@foxmail.com>
2019-10-25 10:15:46 +0800

    tag cache key

--- service/cache/tag.go
+++ service/cache/tag.go
@@ -1,5 +1,12 @@
 package cache
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/boybird/hello/pkg/e"
+)
+
 // Tag tag
 type Tag struct {
 	ID       int
@@ -7,4 +14,26 @@ type Tag struct {
 	State    int
 	PageNum  int
 	PageSize int
+}
+
+// GetTagsKey get tags
+func (t Tag) GetTagsKey() string {
+	keys := []string{
+		e.CACHE_TAG,
+		"LIST",
+	}
+
+	if t.State >= 0 {
+		keys = append(keys, strconv.Itoa(t.State))
+	}
+	if t.PageNum >= 0 {
+		keys = append(keys, strconv.Itoa(t.PageNum))
+	}
+
+	if t.PageSize >= 0 {
+		keys = append(keys, strconv.Itoa(t.PageSize))
+	}
+
+	return strings.Join(keys, "_")
+
 }
